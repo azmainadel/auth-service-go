7391f112a7857f9e6bbf83ce7db3455254756d8c
azmainadel <azmainadel47@gmail.com>
2023-06-01 04:01:57 +0600

    :zap: Hash password using bcrypt

--- services/auth_service.go
+++ services/auth_service.go
@@ -1,9 +1,12 @@
 package services
 
 import (
+	"authentication/config"
 	"authentication/dto"
 	"authentication/interfaces"
 	"authentication/models"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type authenticationService struct {
@@ -14,11 +17,16 @@ func (authService *authenticationService) Login(input dto.LoginInput) {
 	panic("unimplemented")
 }
 
-func (authService *authenticationService) CreateUser(input dto.CreateUserInput) *models.User{
+func (authService *authenticationService) CreateUser(input dto.CreateUserInput) *models.User {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), config.GetBcryptConfig().DEFAULT_COST)
+	if err != nil {
+		panic("Password hashing failed")
+	}
+
 	user := authService.repository.Create(models.User{
 		Username: input.Username,
 		Name:     input.Name,
-		Password: input.Password,
+		Password: string(hashedPassword),
 	})
 
 	return user
