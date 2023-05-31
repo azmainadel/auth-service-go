e68b17a787bc43c9beba2b3c48e858b503b10679
azmainadel <azmainadel47@gmail.com>
2023-06-01 03:39:47 +0600

    :zap: Add User model

---
+++ models/user_model.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	Id       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Name     string    `json:"name"`
+	Password string    `json:"password"`
+}
+
+func (user *User) PreProcess(tx *gorm.DB) (err *error) {
+	user.Id = uuid.New()
+	return
+}
